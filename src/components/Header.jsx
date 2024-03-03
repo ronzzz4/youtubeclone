e95f8e95296da6928167d7d6425d86e0c9394016
Rounak <rounaklongani@gmail.com>
2024-03-03 14:37:52 +0530

    adding placeholder to search bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -57,7 +57,7 @@ const Header = () => {
             <div className="w-10 items-center justify-center hidden group-focus-within:md:flex">
               <IoIosSearch className='text-white text-xl' />
             </div>
-            <input type="text" className='bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]' onChange={(e) => setSearchQuery(e.target.value)} onKeyUp={searchQueryHandler} value={searchQuery}/>
+            <input type="text" placeholder='Search' className='bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]' onChange={(e) => setSearchQuery(e.target.value)} onKeyUp={searchQueryHandler} value={searchQuery}/>
           </div>
           <button className='w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]'>
             <IoIosSearch className='text-white text-xl' />
